fd62dd86a862bfc024d6d84abc0253d9fd838179
Tu Ho <tu.hh@geekup.vn>
2022-08-18 10:16:19 +0700

    Devided function URL

--- WarThunder/urls.py
+++ WarThunder/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from WarThunder import views
 
 urlpatterns = [
-    path('air', views.aircraft_information_api),
-    path('air/<int:id>', views.aircraft_information_api),
-    path('air/<str:name>', views.aircraft_information_api)
+    path('get/air', views.get_aircraft_information_api),
+    path('get/air/<int:id>', views.get_aircraft_information_by_id_api),
+    path('get/air/<str:name>', views.get_aircraft_information_by_name_api),
+    path('post/air', views.post_aircraft_information_api),
+    path('put/air/<int:id>', views.put_aircraft_information_api),
+    path('delete/air/<int:id>', views.delete_aircraft_information_api),
 ]
--- WarThunder/views.py
+++ WarThunder/views.py
@@ -4,35 +4,64 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http.response import JsonResponse
 
 from rest_framework.parsers import JSONParser
-
+from rest_framework.decorators import api_view
 from .serializers import *  # import the serializer we just created
 from .models import *
 
 # Create your views here.
 
 
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
 @csrf_exempt
-def aircraft_information_api(request, id=0, name='name'):
+def get_aircraft_information_api(request):
     if request.method == 'GET':
         vehicles = Aircraft.objects.all().order_by('id')
         vehicles_serializer = AircraftSerializer(vehicles, many=True)
         return JsonResponse(vehicles_serializer.data, safe=False)
-    elif request.method == 'POST':
+
+
+@csrf_exempt
+def get_aircraft_information_by_name_api(request, name='name'):
+    if request.method == 'GET':
+        vehicles = Aircraft.objects.all().order_by('id').filter(name=name)
+        vehicles_serializer = AircraftSerializer(vehicles, many=True)
+        return JsonResponse(vehicles_serializer.data, safe=False)
+
+
+@csrf_exempt
+def get_aircraft_information_by_id_api(request, id=0):
+    if request.method == 'GET':
+        vehicles = Aircraft.objects.all().filter(id=id)
+        vehicles_serializer = AircraftSerializer(vehicles, many=True)
+        return JsonResponse(vehicles_serializer.data, safe=False)
+
+
+@csrf_exempt
+def post_aircraft_information_api(request):
+    if request.method == 'POST':
         vehicle_data = JSONParser().parse(request)
         vehicles_serializer = AircraftSerializer(data=vehicle_data)
         if vehicles_serializer.is_valid():
             vehicles_serializer.save()
-            return JsonResponse('Added Successfully', safe=False)
+            return JsonResponse('Added Aircraft Successfully', safe=False)
         return JsonResponse('Failed to Add', safe=False)
-    elif request.method == 'PUT':
+
+
+@csrf_exempt
+def put_aircraft_information_api(request, id=0):
+    if request.method == 'PUT':
         vehicle_data = JSONParser().parse(request)
         vehicle = Aircraft.objects.get(id=vehicle_data['id'])
         vehicles_serializer = AircraftSerializer(vehicle, data=vehicle_data)
         if vehicles_serializer.is_valid():
             vehicles_serializer.save()
-            return JsonResponse('Updated Successfully', safe=False)
-        return JsonResponse('Failed to Update')
-    elif request.method == 'DELETE':
+            return JsonResponse('Updated Aircraft Successfully', safe=False)
+        return JsonResponse('Failed to Update Aircraft')
+
+
+@csrf_exempt
+def delete_aircraft_information_api(request, id=0):
+    if request.method == 'DELETE':
         vehicle = Aircraft.objects.get(id=id)
         vehicle.delete()
-        return JsonResponse('Deleted Successfully', safe=False)
+        return JsonResponse('Deleted Aircraft Successfully', safe=False)
